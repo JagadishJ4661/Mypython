4dfa907eddaf5d1c362921542943dbcee489f591
JagadishJ4661 <90537734+JagadishJ4661@users.noreply.github.com>
2021-09-12 07:59:42 +0530

    Update Master.py

--- Master.py
+++ Master.py
@@ -1,10 +1,14 @@
 Total = 10000
 Effort = input( "what is your daily effort ?" )
 Effort = int(Effort)
+
 Days = Total // Effort
 Hours = Total % Effort
+
 Months = Days // 30
 Days = Days % 30
+
 Year = Months // 12
 Months = Months % 12 
+
 print(Year , "Years" , Months , "Months" , Days , "Days" , Hours , "Hours" )
